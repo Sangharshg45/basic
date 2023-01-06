4ebde22348264212e3faead845fb772e7a11627b
Sangharshg45 <122082670+Sangharshg45@users.noreply.github.com>
2023-01-06 13:23:00 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -6,15 +6,9 @@ public class Test1
 {
 	public static void main(String[] args) 
 	{
-		// System.setProperty("webdeiver.chrome.driver","./drivers/chromedriver.exe");
 		ChromeDriver driver=new ChromeDriver();
-		//driver.manage().window().maximize();
-		driver.get("https://demo.actitime.com/login.do");
-		String Qspider = driver.getTitle();
-		String Qspider1 = driver.getCurrentUrl();
-		System.out.println(Qspider);
-		System.out.println(Qspider1);
-		
+		driver.manage().window().maximize();
+		driver.get("https://demo.actitime.com/login.do");		
 	}
 
 }
